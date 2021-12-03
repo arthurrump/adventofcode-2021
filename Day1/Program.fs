ff0b504889fcb07847b0c4169fd951d769acf716
Arthur Rump <hello@arthurrump.com>
2021-12-03 19:21:17 +0100

    Finish day 1

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,4 +1,26 @@
-﻿
+﻿open System
+open System.IO
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+#if part1
+
+File.ReadAllLines("input.txt") 
+|> Seq.ofArray
+|> Seq.map (Int32.Parse)
+|> Seq.pairwise
+|> Seq.filter (fun (x, y) -> y > x)
+|> Seq.length
+|> printfn "%d"
+
+#else
+
+File.ReadAllLines("input.txt") 
+|> Seq.ofArray
+|> Seq.map (Int32.Parse)
+|> Seq.windowed 3
+|> Seq.map (Array.sum)
+|> Seq.pairwise
+|> Seq.filter (fun (x, y) -> y > x)
+|> Seq.length
+|> printfn "%d"
+
+#endif
