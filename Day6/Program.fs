0504c7ec0e28866d8eb0f3c24f609da108042542
Arthur Rump <hello@arthurrump.com>
2021-12-06 11:52:29 +0100

    Finish day 6 part 1

---
+++ Day6/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.IO
+
+let initialFish =
+    File.ReadAllText("input.txt").Split(",") |> Array.toList |> List.map int
+
+let step fish =
+    let newFishCount = fish |> List.filter (fun f -> f = 0) |> List.length
+    fish 
+    |> List.map (fun f -> if f = 0 then 6 else f - 1) 
+    |> List.append (List.init newFishCount (fun _ -> 8))
+
+let rec loop until day fish =
+    if day < until then
+        printfn "Day %d: %d fish" day (fish |> List.length)
+        loop until (day + 1) (step fish)
+    else
+        fish
+
+loop 80 0 initialFish
+|> List.length
+|> printfn "%d"
