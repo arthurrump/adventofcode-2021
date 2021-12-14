7d26034181fb402276983bc5cd35409e65633488
Arthur Rump <hello@arthurrump.com>
2021-12-14 09:57:10 +0100

    Finish day 14

--- Day14/Program.fs
+++ Day14/Program.fs
@@ -4,7 +4,15 @@ open System.IO
 let input =
     File.ReadAllLines("input.txt")
 
-let template = input[0].ToCharArray() |> Array.toList
+type Polymer = ((char * char) * bigint) list
+
+let template: Polymer = 
+    ("^" + input[0] + "^").ToCharArray() 
+    |> Array.pairwise
+    |> Array.countBy id
+    |> Array.map (fun (pair, count) -> (pair, bigint count))
+    |> Array.toList
+
 let rules =
     input[2..]
     |> Array.map (fun str -> 
@@ -13,26 +21,30 @@ let rules =
         ((left, right), char insert))
     |> Map.ofArray
 
-let insertElements polymer insertions =
-    let rec insertElements result polymer insertions =
-        match polymer, insertions with
-        | p::polymer, i::insertions -> insertElements (i::p::result) polymer insertions
-        | [ p ], [] -> List.rev (p::result)
-    insertElements [] polymer insertions
-
-let step rules (polymer: char list) =
-    polymer 
-    |> List.pairwise 
-    |> List.map (fun pair -> rules |> Map.find pair)
-    |> insertElements polymer
+let step rules (polymer: Polymer) : Polymer =
+    [ for ((left, right), count) in polymer do
+        match rules |> Map.tryFind (left, right) with
+        | Some insert ->
+            yield ((left, insert), count)
+            yield ((insert, right), count)
+        | None ->
+            yield ((left, right), count) ]
+    |> List.groupBy fst
+    |> List.map (fun (pair, values) -> (pair, values |> List.map snd |> List.sum))
 
 let rec runProcess n rules polymer =
     if n = 0 then polymer
     else runProcess (n-1) rules (step rules polymer)    
 
+#if part1
 runProcess 10 rules template
-|> List.countBy id
-|> List.map snd
+#else
+runProcess 40 rules template
+#endif
+|> List.collect (fun ((left, right), count) -> [ left, count; right, count ])
+|> List.groupBy fst
+|> List.filter (fun (ch, _) -> ch <> '^')
+|> List.map (fun (_, values) -> (values |> List.map snd |> List.sum) / 2I)
 |> List.sort
 |> fun l -> List.last l - List.head l
-|> printfn "%d"
+|> printfn "%A"
