317a6a801c9c8c0ba6a28a38a2875995a893ee95
Arthur Rump <hello@arthurrump.com>
2021-12-21 20:24:54 +0100

    Finish day 21

--- Day21/Program.fs
+++ Day21/Program.fs
@@ -1,9 +1,12 @@
 ﻿open System
+open System.Collections.Generic
 open System.IO
 
 let startPositions =
     File.ReadAllLines("input.txt")
     |> Array.map (fun line -> line.Substring("Player x starting position: ".Length) |> int)
+
+#if part1
 
 let step (players: (int * int)[]) turn nextDie =
     let (position, score) = players[turn]
@@ -27,3 +30,57 @@ let game startPositions =
 let finalPlayers, rounds = game startPositions
 let losingScore = finalPlayers |> Array.minBy snd |> snd
 printfn "%d" (losingScore * rounds * 3)
+
+#else
+
+type [<Struct>] Player = Player of position: int * score: int
+type Universe =
+    { Players: Player[]
+      Turn: int }
+
+module Universe =
+    let hasWinner universe =
+        universe.Players |> Array.exists (fun (Player (_, score)) -> score >= 21)
+    let getWinnerId universe =
+        universe.Players |> Array.findIndex (fun (Player (_, score)) -> score >= 21)
+
+type Universes = (Universe * bigint) list
+
+let rollUniverseCounts = [ (3, 1I); (4, 3I); (5, 6I); (6, 7I); (7, 6I); (8, 3I); (9, 1I) ]
+
+let playUniverseRound (state: Universe, count: bigint) : Universes =
+    rollUniverseCounts
+    |> List.map (fun (roll, universeCount) ->
+        let (Player (position, score)) = state.Players[state.Turn]
+        let position = (position + roll - 1) % 10 + 1
+        let score = score + position
+        { Players = state.Players |> Array.updateAt state.Turn (Player (position, score))
+          Turn = (state.Turn + 1) % state.Players.Length }, count * universeCount)
+
+let playRound (universes: Universes) : Universes =
+    universes
+    |> List.collect playUniverseRound
+    |> List.groupBy fst
+    |> List.map (fun (u, list) -> u, list |> List.sumBy snd)
+
+let playGame startPositions =
+    let rec playGame winCounts universes =
+        let won, universes =
+            playRound universes 
+            |> List.partition (fun (u, _) -> Universe.hasWinner u)
+        let winCounts =
+            won
+            |> List.map (fun (u, count) -> Universe.getWinnerId u, count)
+            |> List.append winCounts
+            |> List.groupBy fst
+            |> List.map (fun (id, list) -> id, list |> List.sumBy snd)
+        if universes |> List.isEmpty
+        then winCounts
+        else playGame winCounts universes
+    playGame [] [ { Players = startPositions |> Array.map (fun pos -> Player (pos, 0)); Turn = 0 }, 1I ]
+
+playGame startPositions
+|> List.sortByDescending snd
+|> printfn "%A" 
+
+#endif
